1adfce2d88e85d60e51e26aef82ae5f294ac7cee
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-05-22 19:48:25 -0400

    updated serializers

--- gadgets_api/serializers.py
+++ gadgets_api/serializers.py
@@ -2,11 +2,20 @@ from rest_framework import serializers
 from .models import Gadget, Location
 
 
+
+
+
+class LocationListingField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.name
+
+
+
 class GadgetSerializer(serializers.ModelSerializer):
-    location_name = serializers.StringRelatedField()
+    #location_name = serializers.StringRelatedField(many=True)
     class Meta:
         model = Gadget
-        fields = ('id', 'name', 'description', 'image_url', 'location', 'location_name')
+        fields = ('id', 'name', 'description', 'image_url', 'location')
 
 
 class LocationSerializer(serializers.ModelSerializer):
@@ -15,3 +24,10 @@ class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = ('id', 'name', 'gadgets')
+        
+    def create(self, validated_data):
+        inventory_data = validated_data.pop('gadgets')
+        location = Location.objects.create(**validated_data)
+        for gadget_data in inventory_data:
+            Gadget.objects.create(location=location, **gadget_data)
+        return location
